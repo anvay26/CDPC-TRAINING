c1fec9b2c28d77d33cab01a60db5734f56c9c1a4
anvay26 <khobragade17anvay@gmail.com>
2026-03-13 11:17:54 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,44 @@
+#  #n=int(input("Enter n: "))
+# x=int(input("Enter n: "))
+# sum=1
+# for i in range(1,n):
+#      sum=sum+(x**i)/i;
+#  print(sum)
+
+# # # op : 2 3= 4
+
+
+#  for i in range(1,5):    # outer loop -rows
+#      for j in range(1,5):   # inner loop - coloumns
+#        print(i,end="")   #i 1111,2222 ,j  1234,1234
+#      print()
+
+#  op : 1111
+#      2222
+ #      3333
+ #      4444
+
+# n=1
+# for i in range(1,5):
+#     for j in range(1,5):
+#         print(n,end="\t")
+#         n=n+1
+#     print()
+
+# op:  1 2 3 4 
+#      5 6 7 8
+#      9 10 11  12
+#     13 14 15 16   
+
+# n=65
+# for i in range(1,5):
+#      for j in range(1,5):
+#          print(chr(n),end="\t")
+#          n=n+1
+#      print()
+
+for i in range(1,5):
+     for j in range(1,i+1):
+         print(i,end="")
+        
+     print()
