a9049d7e3e3910852bda081e8227a92c59466a03
anvay26 <khobragade17anvay@gmail.com>
2026-03-13 11:19:56 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,68 @@
+# def add(x,y):
+#     res=x+y
+#     return res
+
+# if __name__== '__main__':
+#     a=int(input("Enter value: "))
+#     b=int(input("Enter value: "))
+#     res=add(a,b)
+#     print(res)
+
+
+# def arithmatic(x,y):
+#     res1=x+y
+#     res2=x-y
+#     res3=x*y
+#     res4=x/y
+    
+# if __name__== '__main__':
+#     a=int(input("Enter value: "))
+#     b=int(input("Enter value: "))
+#     r1 ,r2 ,r3 ,r4 =arithmatic(a,b)
+#     print(r1,r2,r3,r4)
+
+# list
+# ls=[]
+# print(type(ls))
+
+# ls=list()
+# print(type(ls))
+
+# ls=[11,22,33,44,55,66]
+# print(ls)
+# for i in range(len(ls)):
+#     print(ls[i])
+
+# for x in ls:
+#     print(x)
+
+# append
+# ls=[11,22,33,44,55,66]
+# ls.append(77)
+# ls.append(88)
+# print(ls)
+# print(ls[0])
+
+# ls=[11,22,33,44,55,66]
+# print(ls)
+# print(ls[0])
+# print(ls[-8])
+# ls[5]=123
+# print(ls)
+
+# ls=[11,22,33,44,55,66]
+# print(ls)
+# print(ls[2:5])
+# print(ls[3:6])
+# print(ls[:6])
+# print(ls[2:])
+# print(ls[:])
+# print(ls[::1])
+# print(ls[::2])   #alternate list
+# print(ls[::-1])  #reverse the array (slicing technique)
+
+# string
+s="Anvay"
+print(s[2:4])
+rev=s[::-1]
+print(rev)
