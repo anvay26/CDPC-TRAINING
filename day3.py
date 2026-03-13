9bd06532ab9279f74f1ac440f5dd270cd5580738
anvay26 <khobragade17anvay@gmail.com>
2026-03-13 11:19:07 +0530

    Update day3.py

--- day3.py
+++ day3.py
@@ -37,8 +37,9 @@
 #          n=n+1
 #      print()
 
-for i in range(1,5):
-     for j in range(1,i+1):
-         print(i,end="")
+# for i in range(1,5):
+     # for j in range(1,i+1):
+         # print(i,end="")
         
-     print()
+     # print()
+
